36bd32d6643926cf8cce7fb367f369dc52e134d4
markpal <marekp1980@gmail.com>
2023-12-09 14:37:56 +0100

    mcc raw rplus

--- raw/mcc.raw.c
+++ raw/mcc.raw.c
@@ -1,9 +1,9 @@
         #pragma scop
-        if(N>=1 && l>=0 && l<=5)
+        if(N>=1)
         for(i=N-1; i>=0; i--){
          for(j=i+1; j<N; j++){
             Q1[i][j] =  Q1[i][j-1];
-           for(k=0; k<j-i-l; k++){
+           for(k=0; k<j-i; k++){
              Qbp1[k+i][j] = Q1[k+i+1][j-1] * ERT * paired(k+i,j-1);
              Q1[i][j] +=  Q1[i][k+i] * Qbp1[k+i][j];
            }
