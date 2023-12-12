fd0cfe72d00e462ac2b95b0e911c4ce8257f59f3
markpal <marekp1980@gmail.com>
2023-12-12 08:32:34 +0100

    Update knuth_cuda.cu

--- knuth_cuda.cu
+++ knuth_cuda.cu
@@ -1,74 +1,166 @@
-#include <iostream>
-#include <algorithm>  // Include the algorithm header for the min function
 #include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <chrono>
 
-__global__ void cudaKernel(int n, int** w, int** ck) {
-    int w0 = blockIdx.x * blockDim.x + threadIdx.x + 2;
-    if (w0 < n) {
-        for (int h0 = -n + w0; h0 < 0; h0 += 1) {
-            for (int i2 = -h0 + 1; i2 < w0 - h0; i2 += 1) {
-                ck[(w0 - h0) * n + (-h0)] = std::min(ck[(w0 - h0) * n + (-h0)], (w[(w0 - h0) * n + (-h0)] + ck[(w0 - h0) * n + (-h0) - i2]) + ck[(w0 - h0) * n + (-h0) + i2]);
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define CHUNK_SIZE 4
+
+// Device paired function
+__device__ int paired_device(int a, int b) {
+    // Replace this with your implementation of the paired function
+    return a * b;  // Example implementation, replace with the actual logic
+}
+
+int ERT = 1;
+
+// Host paired function
+int paired_host(int a, int b) {
+    // Replace this with your implementation of the paired function
+    return a * b;  // Example implementation, replace with the actual logic
+	
+}
+
+// Device max function
+__device__ int max_device(int a, int b) {
+    return MAX(a, b);
+}
+
+
+__global__ void computeQ(int N, int c1, int** d_Q1, int** d_Qbp1) {
+    int globalThreadIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    int c3_start = globalThreadIdx * CHUNK_SIZE;
+    int c3_end = c3_start + CHUNK_SIZE;
+	
+	int ERT=1;
+
+    for (int c3 = c3_start; c3 < c3_end && c3 < (c1 + 1) / 2; c3++) {
+        if (c3 >= max_device(0, -N + c1 + 1)) {
+            d_Q1[N-c1+c3-1][N-c1+2*c3] = d_Q1[N-c1+c3-1][N-c1+2*c3-1];
+            for (int c5 = 0; c5 <= c3; c5++) {
+                d_Qbp1[c5+(N-c1+c3-1)][N-c1+2*c3] = d_Q1[c5+(N-c1+c3-1)+1][N-c1+2*c3-1] * ERT * paired_device(c5+(N-c1+c3-1),N-c1+2*c3-1);
+                d_Q1[N-c1+c3-1][N-c1+2*c3] += d_Q1[N-c1+c3-1][c5+(N-c1+c3-1)] * d_Qbp1[c5+(N-c1+c3-1)][N-c1+2*c3];
             }
         }
     }
 }
 
-// Rest of the code remains unchanged...
-
-
 int main() {
-    int n = 100;  // Replace with your desired value for n
+    int N = 5000;  // Example size
+    int **h_Q1, **d_Q1, **h_Qbp1, **d_Qbp1, **cpu_Q1, **cpu_Qbp1;
+    int *d_Q1_data, *d_Qbp1_data;
 
-    // Allocate and initialize data on the host
-    int** h_w = new int*[n];
-    int** h_ck = new int*[n];
-    for (int i = 0; i < n; ++i) {
-        h_w[i] = new int[n];
-        h_ck[i] = new int[n];
-        // Initialize h_w[i] and h_ck[i] as needed
+    // Allocate and initialize host memory
+    h_Q1 = (int**)malloc(N * sizeof(int*));
+    h_Qbp1 = (int**)malloc(N * sizeof(int*));
+    cpu_Q1 = (int**)malloc(N * sizeof(int*));
+    cpu_Qbp1 = (int**)malloc(N * sizeof(int*));
+
+    for (int i = 0; i < N; i++) {
+        h_Q1[i] = (int*)malloc(N * sizeof(int));
+        h_Qbp1[i] = (int*)malloc(N * sizeof(int));
+        cpu_Q1[i] = (int*)malloc(N * sizeof(int));
+        cpu_Qbp1[i] = (int*)malloc(N * sizeof(int));
+
+        for (int j = 0; j < N; j++) {
+            h_Q1[i][j] = rand() % 100;  // Example initialization
+            h_Qbp1[i][j] = rand() % 100;  // Example initialization
+            cpu_Q1[i][j] = h_Q1[i][j];
+            cpu_Qbp1[i][j] = h_Qbp1[i][j];
+        }
     }
 
-    // Allocate memory on the device
-    int** d_w, **d_ck;
-    cudaMalloc((void**)&d_w, n * sizeof(int*));
-    cudaMalloc((void**)&d_ck, n * sizeof(int*));
+    // Allocate device memory
+    cudaMalloc(&d_Q1_data, N * N * sizeof(int));
+    cudaMalloc(&d_Qbp1_data, N * N * sizeof(int));
+    cudaMalloc(&d_Q1, N * sizeof(int*));
+    cudaMalloc(&d_Qbp1, N * sizeof(int*));
 
-    // Copy data from host to device
-    for (int i = 0; i < n; ++i) {
-        cudaMalloc((void**)&d_w[i], n * sizeof(int));
-        cudaMalloc((void**)&d_ck[i], n * sizeof(int));
-        cudaMemcpy(d_w[i], h_w[i], n * sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_ck[i], h_ck[i], n * sizeof(int), cudaMemcpyHostToDevice);
+    int **h_Q1_array = (int **)malloc(N * sizeof(int *));
+    int **h_Qbp1_array = (int **)malloc(N * sizeof(int *));
+
+    for (int i = 0; i < N; i++) {
+        h_Q1_array[i] = d_Q1_data + i * N;
+        h_Qbp1_array[i] = d_Qbp1_data + i * N;
     }
 
-    // Set up the grid and block dimensions
-    int blockSize = 256;  // Adjust as needed
-    int gridSize = (n - 2 + blockSize - 1) / blockSize;
+    cudaMemcpy(d_Q1, h_Q1_array, N * sizeof(int *), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Qbp1, h_Qbp1_array, N * sizeof(int *), cudaMemcpyHostToDevice);
 
-    // Launch the CUDA kernel
-    cudaKernel<<<gridSize, blockSize>>>(n, d_w, d_ck);
-
-    // Copy the result back from device to host
-    for (int i = 0; i < n; ++i) {
-        cudaMemcpy(h_ck[i], d_ck[i], n * sizeof(int), cudaMemcpyDeviceToHost);
+    // Copy data to device
+    for (int i = 0; i < N; i++) {
+        cudaMemcpy(h_Q1_array[i], h_Q1[i], N * sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(h_Qbp1_array[i], h_Qbp1[i], N * sizeof(int), cudaMemcpyHostToDevice);
     }
 
-    // Free allocated memory on the device
-    for (int i = 0; i < n; ++i) {
-        cudaFree(d_w[i]);
-        cudaFree(d_ck[i]);
-    }
-    cudaFree(d_w);
-    cudaFree(d_ck);
+    // GPU computation
+    int threadsPerBlock = 256;
+    int numBlocks = (N / (threadsPerBlock * CHUNK_SIZE)) + 1;
 
-    // Free allocated memory on the host
-    for (int i = 0; i < n; ++i) {
-        delete[] h_w[i];
-        delete[] h_ck[i];
+    auto gpu_start = std::chrono::high_resolution_clock::now();
+
+    for (int c1 = 1; c1 < 2 * N - 2; c1 += 1) {
+        computeQ<<<numBlocks, threadsPerBlock>>>(N, c1, d_Q1, d_Qbp1);
+        cudaDeviceSynchronize();
     }
-    delete[] h_w;
-    delete[] h_ck;
+
+    auto gpu_end = std::chrono::high_resolution_clock::now();
+
+    // Copy results back to host
+    for (int i = 0; i < N; i++) {
+        cudaMemcpy(h_Q1[i], h_Q1_array[i], N * sizeof(int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_Qbp1[i], h_Qbp1_array[i], N * sizeof(int), cudaMemcpyDeviceToHost);
+    }
+
+    // CPU computation
+    auto cpu_start = std::chrono::high_resolution_clock::now();
+
+    for (int c1 = 1; c1 < 2 * N - 2; c1 += 1) {
+        for (int c3 = max(0, -N + c1 + 1); c3 < (c1 + 1) / 2; c3++) {
+            cpu_Q1[N-c1+c3-1][N-c1+2*c3] = cpu_Q1[N-c1+c3-1][N-c1+2*c3-1];
+            for (int c5 = 0; c5 <= c3; c5++) {
+                cpu_Qbp1[c5+(N-c1+c3-1)][N-c1+2*c3] = cpu_Q1[c5+(N-c1+c3-1)+1][N-c1+2*c3-1] * ERT * paired_host(c5+(N-c1+c3-1),N-c1+2*c3-1);
+                cpu_Q1[N-c1+c3-1][N-c1+2*c3] += cpu_Q1[N-c1+c3-1][c5+(N-c1+c3-1)] * cpu_Qbp1[c5+(N-c1+c3-1)][N-c1+2*c3];
+            }
+        }
+    }
+
+    auto cpu_end = std::chrono::high_resolution_clock::now();
+
+    // Validate results
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            assert(h_Q1[i][j] == cpu_Q1[i][j]);
+            assert(h_Qbp1[i][j] == cpu_Qbp1[i][j]);
+        }
+    }
+
+    // Print timings
+    std::chrono::duration<double, std::milli> gpu_duration = gpu_end - gpu_start;
+    std::chrono::duration<double, std::milli> cpu_duration = cpu_end - cpu_start;
+    printf("GPU calculation took: %f ms\n", gpu_duration.count());
+    printf("CPU calculation took: %f ms\n", cpu_duration.count());
+
+    // Free device memory
+    cudaFree(d_Q1_data);
+    cudaFree(d_Qbp1_data);
+    cudaFree(d_Q1);
+    cudaFree(d_Qbp1);
+
+    // Free host memory
+    for (int i = 0; i < N; i++) {
+        free(h_Q1[i]);
+        free(h_Qbp1[i]);
+        free(cpu_Q1[i]);
+        free(cpu_Qbp1[i]);
+    }
+    free(h_Q1);
+    free(h_Qbp1);
+    free(cpu_Q1);
+    free(cpu_Qbp1);
+    free(h_Q1_array);
+    free(h_Qbp1_array);
 
     return 0;
 }
-
